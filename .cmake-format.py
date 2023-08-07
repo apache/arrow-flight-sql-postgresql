0196e6cf22a9ad48188331e1c03e775cbb70bc43
Sutou Kouhei <kou@clear-code.com>
2023-08-07 16:56:45 +0900

    Add format lint CI (#45)
    Closes GH-44

--- .cmake-format.py
+++ .cmake-format.py
@@ -15,5 +15,5 @@
 # specific language governing permissions and limitations
 # under the License.
 
-markup:
-  first_comment_is_literal: true
+with section("markup"):
+  first_comment_is_literal = True
