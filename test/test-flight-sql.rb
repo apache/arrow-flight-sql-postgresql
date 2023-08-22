22ac70f8470143403069e9df3f2cf89f13fa62a9
Sutou Kouhei <kou@clear-code.com>
2023-08-22 15:23:51 +0900

    Add support for UInt16
    Closes GH-52

--- test/test-flight-sql.rb
+++ test/test-flight-sql.rb
@@ -81,11 +81,12 @@ SELECT * FROM data
     RESULT
   end
 
-  data("int8",  ["smallint", Arrow::Int8Array,  [1, -2, 3]])
-  data("int16", ["smallint", Arrow::Int16Array, [1, -2, 3]])
-  data("int32", ["integer",  Arrow::Int32Array, [1, -2, 3]])
-  data("int64", ["bigint",   Arrow::Int64Array, [1, -2, 3]])
-  data("uint8", ["smallint", Arrow::UInt8Array, [1,  2, 3]])
+  data("int8",   ["smallint", Arrow::Int8Array,   [1, -2, 3]])
+  data("int16",  ["smallint", Arrow::Int16Array,  [1, -2, 3]])
+  data("int32",  ["integer",  Arrow::Int32Array,  [1, -2, 3]])
+  data("int64",  ["bigint",   Arrow::Int64Array,  [1, -2, 3]])
+  data("uint8",  ["smallint", Arrow::UInt8Array,  [1,  2, 3]])
+  data("uint16", ["smallint", Arrow::UInt16Array, [1,  2, 3]])
   def test_insert_type
     unless flight_sql_client.respond_to?(:prepare)
       omit("red-arrow-flight-sql 14.0.0 or later is required")
